75f5c501c7a8315cb07ff458878f9baeb7c50b35
eric <eacraft13@gmail.com>
2017-12-12 15:05:28 -0600

    Add json middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 'use strict';
 
-var conf     = require('./config/app');
+var config   = require('./config/app');
 var express  = require('express'),
     app      = express();
 var leads    = require('./routes/leads');
 var resales  = require('./routes/resales');
 var supplies = require('./routes/supplies');
 
+app.use(express.json());
 app.use(require('res-error'));
 
 app.use('/leads', leads);
 app.use('/resales', resales);
 app.use('/supplies', supplies);
 
-app.listen(conf.port);
+app.listen(config.port);
