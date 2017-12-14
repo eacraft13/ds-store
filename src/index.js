260ce0366837ad2b2bd4b2edd031f47bd2a2f26a
eric <eacraft13@gmail.com>
2017-12-14 15:47:22 -0600

    Add listings and sales routes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ var config   = require('./config/app');
 var express  = require('express'),
     app      = express();
 var leads    = require('./routes/leads');
+var listings = require('./routes/listings');
 var resales  = require('./routes/resales');
+var sales    = require('./routes/sales');
 var supplies = require('./routes/supplies');
 
 app.use(express.json());
 app.use(require('res-error'));
 
 app.use('/leads', leads);
+app.use('/listings', listings);
 app.use('/resales', resales);
+app.use('/sales', sales);
 app.use('/supplies', supplies);
 
 app.listen(config.port);
