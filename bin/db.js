6d58e96c38ce32452727844969c2fb9445b67134
eric <eacraft13@gmail.com>
2017-12-15 03:38:20 -0600

    Create db initialization script

---
+++ bin/db.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var _ = require('lodash');
+var config = require('../src/config/db'),
+    r = require('rethinkdbdash')(config);
+var schemas;
+
+schemas = [
+    {
+        name: 'lead',
+        primaryKey: 'resaleId'
+    },
+    {
+        name: 'listing',
+        primaryKey: 'resaleId'
+    },
+    {
+        name: 'resale',
+        primaryKey: 'eBayId'
+    },
+    {
+        name: 'sale',
+        primaryKey: 'resaleId'
+    },
+    {
+        name: 'supply'
+    }
+];
+
+r
+.dbList()
+.run()
+.then(function (dbs) {
+    if (_.includes(dbs, config.db))
+        return true;
+    else
+        return r.dbCreate(config.db);
+})
+.then(function () {
+    return r.db(config.db).tableList();
+})
+.then(function (tables) {
+    return _.filter(schemas, function (options) {
+        return !_.includes(tables, options.name);
+    });
+})
+.then(function (tables) {
+    return Promise.all(
+        _.map(tables, function (options) {
+            return r.db(config.db)
+                .tableCreate(options.name, { primaryKey: options.primaryKey })
+                .run();
+        })
+    );
+})
+.then(function (result) {
+    console.log(result);
+    process.exit();
+})
+.error(function (err) {
+    console.log(err);
+    process.exit(1);
+});
+
+
