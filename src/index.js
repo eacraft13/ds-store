4ba64c601ec7a1610a2a3cae1abb1fc3e00742a7
eric <eacraft13@gmail.com>
2017-12-28 17:03:16 -0600

    Setup leads and supplies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var Lead     = require('./models/Lead');
 var Listing  = require('./models/Listing');
 var config   = require('./config/server');
 var cors     = require('cors');
 var express  = require('express'),
     app      = express();
+var leads    = require('./routes/leads');
 var listings = require('./routes/listings');
 var morgan   = require('morgan');
 var r        = require('./r');
@@ -24,7 +26,20 @@ app.use('/listings/:listing_id/snipes', function (req, res, next) {
         .get(req.params.listing_id)
         .then(function (listing) {
             if (!listing)
-                return res.error(400, 'Listing not found');
+                return res.error(404, 'Listing not found');
+
+            req.resale = listing;
+            req.model = Listing;
+
+            return next();
+        });
+}, snipes);
+app.use('/listings/:listing_id/supplies', function (req, res, next) {
+    return Listing
+        .get(req.params.listing_id)
+        .then(function (listing) {
+            if (!listing)
+                return res.error(404, 'Listing not found');
 
             req.resale = listing;
             req.model = Listing;
@@ -36,6 +51,33 @@ app.use('/listings/:listing_id/snipes', function (req, res, next) {
 /**
  * Leads
  */
+app.use('/leads', leads);
+app.use('/leads/:lead_id/snipes', function (req, res, next) {
+    return Lead
+        .get(req.params.lead_id)
+        .then(function (lead) {
+            if (!lead)
+                return res.error(404, 'Lead not found');
+
+            req.resale = lead;
+            req.model = Lead;
+
+            return next();
+        });
+}, snipes);
+app.use('/leads/:lead_id/supplies', function (req, res, next) {
+    return Lead
+        .get(req.params.lead_id)
+        .then(function (lead) {
+            if (!lead)
+                return res.error(404, 'Lead not found');
+
+            req.resale = lead;
+            req.model = Lead;
+
+            return next();
+        });
+}, snipes);
 
 app.listen(config.port);
 
--- src/models/supply.js
+++ src/models/supply.js
@@ -1,48 +1,52 @@
-'use strict';
-
 /**
- * Supplies are actual or potential items to drop ship from a merchant
+ * Supplies are listings from other eBay stores
  */
 
-var r = require('../r');
-var schema;
-var table = 'listing';
-var Joi = require('joi');
-var Promise = require('bluebird');
-var Supply = {};
+'use strict';
 
+var Joi     = require('joi');
+var Promise = require('bluebird');
+var Supply   = {};
+var _       = require('lodash');
+var ebay    = require('ebay-dev-api')(require('../../private/ebay'));
+var r       = require('../r');
+var schema;
+var table   = 'supplies';
+
+/**
+ * Supply schema
+ */
 schema = Joi.object().keys({
     id: Joi.string().required(),
 
-    data: Joi.object().allow(null),
-    merchantId: Joi.string().required(),
-    merchantName: Joi.string().required(),
+    ebay: Joi.object().keys({
+        finding: Joi.object().allow(null),
+        shopping: Joi.object().required(),
+    }),
 
     createdAt: Joi.date().timestamp('unix').default(Date.now(), 'created at date'),
     updatedAt: Joi.date().timestamp('unix').default(Date.now(), 'updated at date'),
 });
 
 /**
- * Generates id
- */
-Supply.generateId = function (merchantName, merchantId) {
-    return `${merchantName.toLowerCase()}-${merchantId}`;
-};
-
-/**
  * Get all
  */
 Supply.getAll = function (filters) {
-    var query = r.table(table);
+    if (Array.isArray(filters) && filters.length === 0)
+        return r
+            .table(table)
+            .filter(null)
+            .run();
 
     if (filters)
-        query.filter(filters);
+        return r
+            .table(table)
+            .filter(filters)
+            .run();
 
-    return query
-        .run()
-        .then(function (cursor) {
-            return cursor.toArray();
-        });
+    return r
+        .table(table)
+        .run();
 };
 
 /**
@@ -69,21 +73,175 @@ Supply.destroy = function (id) {
 /**
  * Create (or update)
  */
-Supply.createOrUpdate = function (apiName, apiId) {
+Supply.createOrUpdate = function (supply) {
+    var joi;
+
+    delete supply.createdAt;
+    delete supply.updatedAt;
+
+    supply.id = ebay.shopping.generateId(supply.ebay.shopping);
+    joi = Joi.validate(supply, schema);
+
+    if (joi.error)
+        return Promise.reject(new Error(joi.error));
+
+    return r
+        .table(table)
+        .insert(joi.value, {
+            conflict: function (id, oldDoc, newDoc) {
+                return oldDoc.merge(newDoc, { updatedAt: Date.now() });
+            },
+            returnChanges: true
+        })
+        .run();
+};
+
+/**
+ * Add (by itemId)
+ */
+Supply.add = function (itemId) {
+    var self = this;
+
+    return ebay
+        .shopping
+        .getMultipleItems([itemId])
+        .then(function (items) {
+            return _.map(items, function (item) {
+                return {
+                    ebay: {
+                        shopping: item
+                    }
+                };
+            });
+        })
+        .then(function (resales) {
+            return _(resales)
+                .map(function (resale) {
+                    var variations = ebay.shopping.explodeVariations(resale.ebay.shopping);
+
+                    return _.map(variations, function (variation) {
+                        var clone = _.cloneDeep(resale);
+
+                        clone.ebay.shopping = variation;
+
+                        return clone;
+                    });
+                })
+                .flatten()
+                .valueOf();
+        })
+        .then(function (resales) {
+            return Promise
+                .all(
+                    _.map(resales, function (resale) {
+                        return self.createOrUpdate(resale);
+                    })
+                );
+        });
+};
+
+/**
+ * Remove (by itemId)
+ */
+Supply.remove = function (itemId) {
+    return ebay
+        .shopping
+        .getMultipleItems([itemId])
+        .then(function (items) {
+            return _(items)
+                .map(function (item) {
+                    var variations = ebay.shopping.explodeVariations(item);
+
+                    return _.map(variations, function (variation) {
+                        return ebay.shopping.generateId(variation);
+                    });
+                })
+                .flatten()
+                .valueOf();
+        });
 };
 
 /**
  * Sync all
  */
-Supply.sync = function () {
+Supply.sync = function (ids) {
+    var explodedIds;
+    var self = this;
 
+    explodedIds = _.map(ids, function (id) {
+        return ebay.shopping.explodeId(id);
+    });
+
+    return ebay
+        .shopping
+        .getMultipleItems(_.map(explodedIds, 'itemId'))
+        .then(function (items) {
+            return _.map(items, function (item) {
+                return {
+                    ebay: {
+                        shopping: item
+                    }
+                };
+            });
+        })
+        .then(function (resales) {
+            return _(resales)
+                .map(function (resale) {
+                    var resaleId, variation;
+
+                    resaleId = _.find(explodedIds, function (explodedId) {
+                        return explodedId.itemId = resale.ebay.shopping.ItemID;
+                    });
+                    variation = ebay.shopping.getVariation(resale.ebay.shopping, resaleId.variationHash);
+
+                    resale.ebay.shopping = variation;
+
+                    return resale;
+                })
+                .valueOf();
+        })
+        .then(function (resales) {
+            return Promise
+                .all(
+                    _.map(resales, function (resale) {
+                        return self.createOrUpdate(resale);
+                    })
+                );
+        });
 };
 
 /**
  * Refresh (by id)
  */
 Supply.refresh = function (id) {
+    var explodedId;
+    var self = this;
 
+    if (!id)
+        return Promise.resolve({ unchanged: 1 });
+
+    explodedId = ebay.shopping.explodeId(id);
+
+    return ebay
+        .shopping
+        .getMultipleItems([explodedId.itemId])
+        .then(function (items) {
+            return {
+                ebay: {
+                    shopping: items[0]
+                }
+            };
+        })
+        .then(function (supply) {
+            var variation = ebay.shopping.getVariation(supply.ebay.shopping, explodedId.variationHash);
+
+            supply.ebay.shopping = variation;
+
+            return supply;
+        })
+        .then(function (supply) {
+            return self.createOrUpdate(supply);
+        });
 };
 
 module.exports = Supply;
